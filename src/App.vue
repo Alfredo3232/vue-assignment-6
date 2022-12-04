55c9f80520bb688d2f41940e581bfd3137ed5ae7
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-12-04 15:45:12 -0600

    completed task 1 and 2

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-if="bol">{{ goal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="toggleButton">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,8 +13,27 @@
 </template>
 
 <script>
-export default {
+import { ref } from 'vue';
 
+export default {
+  setup() {
+    let goal = ref('Build A PC');
+    let bol = ref(true);
+
+    function toggleButton() {
+      if (bol.value) {
+        bol.value = false;
+      } else {
+        bol.value = true;
+      }
+    }
+
+    return {
+      goal,
+      bol,
+      toggleButton
+    }
+  }
 };
 </script>
 
